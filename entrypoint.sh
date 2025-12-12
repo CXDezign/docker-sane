822315685474ac0a04936290b14c0f9cb6af4498
CXDezign <29733018+CXDezign@users.noreply.github.com>
2025-12-13 00:44:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Environment Variables
 set -e
 
-# Execute
-exec /usr/sbin/saned -l -e -n
+# Sockets
+/usr/bin/systemctl enable saned.socket
 
 # User Configuration
 usermod -a -G scanner root
@@ -15,9 +15,12 @@ echo "${ALLOW_IP}" >> /etc/sane.d/saned.conf
 echo "net" >> /etc/sane.d/dll.conf
 echo "${SANE_BACKEND_DLL}" >> /etc/sane.d/dll.conf
 echo "${SERVER_IP}" >> /etc/sane.d/net.conf
-echo "sane-port       6566/tcp" >> /etc/services
+echo "sane-port 6566/tcp" >> /etc/services
 
 # Restore Configurations
 if [ ! -f /etc/sane.d/saned.conf ]; then
     cp -rpn /etc/sane.d.bak/* /etc/sane.d/
 fi
+
+# Execute
+exec /usr/sbin/saned -l -e -n

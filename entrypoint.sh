73ca53d08bf6312ad5633e2071a2542244a091b1
CXDezign <29733018+CXDezign@users.noreply.github.com>
2025-12-12 23:36:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,12 @@ set -e
 # Start SANE Daemon
 saned -l -e
 
+# Add root user to SANE group
+usermod -a -G scanner root
+
 # Default SANE Configuration
 echo "RUN=yes" >> /etc/default/saned
-echo "data_portrange = ${SANE_PORT}" >> /etc/sane.d/cupsd.conf
 echo "${ALLOW_IP}" >> /etc/sane.d/cupsd.conf
 echo "net" >> /etc/sane.d/dll.conf
 echo "${SANE_BACKEND_DLL}" >> /etc/sane.d/dll.conf
 echo "${SERVER_IP}" >> /etc/sane.d/net.conf
-
-# Add root user to SANE group
-usermod -a -G scanner root

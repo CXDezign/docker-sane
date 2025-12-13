a684c0a9f48ec8f07fac2d1d334ab686cabec1ef
CXDezign <29733018+CXDezign@users.noreply.github.com>
2025-12-13 01:00:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,29 @@ set -e
 usermod -a -G scanner root
 
 # Default Configuration
-echo "RUN=yes" >> /etc/default/saned
-echo "${ALLOW_IP}" >> /etc/sane.d/saned.conf
-echo "net" >> /etc/sane.d/dll.conf
-echo "${SANE_BACKEND_DLL}" >> /etc/sane.d/dll.conf
-echo "${SERVER_IP}" >> /etc/sane.d/net.conf
-echo "sane-port 6566/tcp" >> /etc/services
+if [ ! -f /etc/default/saned ]; then
+    echo "RUN=yes" >> /etc/default/saned
+    echo "RUN_AS_USER=root" >> /etc/default/saned
+fi
+if [ -f /etc/sane.d/saned.conf ]; then
+    echo "${ALLOW_IP}" >> /etc/sane.d/saned.conf
+fi
+if [ -f /etc/sane.d/dll.conf ]; then
+    echo "net" >> /etc/sane.d/dll.conf
+    echo "${SANE_BACKEND_DLL}" >> /etc/sane.d/dll.conf
+fi
+if [ -f /etc/sane.d/net.conf ]; then
+    echo "${SERVER_IP}" >> /etc/sane.d/net.conf
+fi
+if [ ! -f /etc/services ]; then
+    echo "sane-port 6566/tcp" >> /etc/services
+fi
 
 # Restore Configurations
 if [ ! -f /etc/sane.d/saned.conf ]; then
     cp -rpn /etc/sane.d.bak/* /etc/sane.d/
 fi
 
+wait
 # Execute
-exec /usr/sbin/saned -l -e -n
+#exec /usr/sbin/saned -l -e -n
